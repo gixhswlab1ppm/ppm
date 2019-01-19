646ddeff93e7cc7a7e47aac9a6a95ff3e53e4c82
Cutuy <cutuy@outlook.com>
2019-01-18 19:52:03 -0800

    Add new datasets; impl viz for spectrum comparison

--- fft_test.py
+++ fft_test.py
@@ -1,60 +1,77 @@
 # acc file name
-acc_path = 'accelerometer.csv'
-acc_feature = 'Absolute acceleration (m/s^2)'
+acc_path = 'gyroscope.csv'
+# acc_feature = 'Absolute acceleration (m/s^2)'
+acc_feature = 'Absolute (rad/s)'
 acc_sample_rate = 0.005  # 5ms
 
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
-df = pd.read_csv(acc_path, header=0)
 
-df.plot()
-plt.show()
-
-y = df[acc_feature].values
 
 
 def fft(y):
     y -= np.mean(y)
     freq_map = np.fft.rfft(y)
     freq_map_abs = np.abs(freq_map)
-    freq_frequencies = np.fft.rfftfreq(len(freq_map), d=acc_sample_rate)
+    freq_frequencies = np.fft.rfftfreq(len(y), d=acc_sample_rate)
 
-    plt.figure()
-    plt.plot(freq_map_abs)
+    #plt.figure()
+    #plt.plot(freq_frequencies, freq_map_abs/freq_map_abs.max(), alpha=.2)
+    #plt.show()
 
     # sort by abs, but take original spectrum (w/ imaginary part)
     freq_reserve_idx = np.argsort(freq_map_abs)[-10:][::-1]
     print("Window size: ", len(y)*1000*acc_sample_rate, "ms")
     print("Main freqs (Hz): ", freq_frequencies[freq_reserve_idx])
-    print("Main amps  (rel to max): ", freq_map_abs[freq_reserve_idx]/freq_map_abs[freq_reserve_idx].max())
+    print("Main amps  (rel to max): ",
+          freq_map_abs[freq_reserve_idx]/freq_map_abs[freq_reserve_idx].max())
     print()
     # print(freq_reserve_idx)
+    
 
     freq_mask = np.ones(len(freq_map), dtype=bool)
     freq_mask[freq_reserve_idx] = False
 
-    plt.scatter(freq_reserve_idx, freq_map_abs[freq_reserve_idx], color='red')
+    #plt.scatter(freq_reserve_idx, freq_map_abs[freq_reserve_idx], color='red')
 
     freq_map_filtered = freq_map.copy()
     freq_map_filtered[freq_mask] = 0
     y_filtered = np.fft.irfft(freq_map_filtered)
 
-    plt.figure()
-    plt.plot(y_filtered)
-    plt.plot(y)
+    plt.scatter(freq_frequencies, np.abs(freq_map_filtered)/freq_map_abs.max(), alpha=.5)
 
+    #plt.figure()
+    #plt.plot(y_filtered)
+    #plt.plot(y)
+    return (freq_frequencies, freq_map_abs) # x: frequency, y: amplititude
 
-fft(y)
-# single swing
-fft(y[3357:3726])
-fft(y[5000:6000])
-fft(y[5000:7000])
-fft(y[2000:6000])
+df1 = pd.read_csv('gyroscope.csv', header=0)
+y1 = df1[acc_feature].values
+fft(y1)
 
+y2 = y1[2000:6000]
+fft(y2)
+
+#df.plot()
 #plt.show()
+
+
+
+# fft(y[3357:3726]) # single swing
+# fft(y[5000:6000])
+# fft(y[5000:7000])
+# fft(y[2000:6000])
+plt.show()
+
+plt.figure(2)
+plt.plot(y1, alpha=.5)
+plt.plot(y2, alpha=.5)
+plt.show()
+
 
 
 # TODO: adjacent frequencies combination
