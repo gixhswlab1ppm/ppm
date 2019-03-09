e51c85d63925357abed9c4d32418183dcc52ec2d
Cutuy <cutuy@outlook.com>
2019-03-08 19:30:05 -0800

    Impl multi display screens

--- device/display.py
+++ device/display.py
@@ -12,22 +12,52 @@ dev_lock = threading.Lock()
 
 time_font = ImageFont.truetype('ArialNova.ttf', size=12)
 
-
-field_locations = [[36,8,96,32], [36,36,96,60], [124,8,212,32], [124,36,212,60], [164,0,212,8]]
+# slot 0, 1, 2, 3, time, pause
+field_locations = [[36,8,96,32], [36,36,96,60], [124,8,212,32], [124,36,212,60], [164,0,212,8], [8,64,96,88]]
 field_font = ImageFont.truetype('ArialNova.ttf', size=24)
+title_small = ImageFont.truetype('ArialNova.ttf', size=12)
 last_img = Image.new('1', (epd2in13d.EPD_HEIGHT, epd2in13d.EPD_WIDTH), 255)
 
-
-def _render_template():
-    global last_img
-    # 4 vals of `box` has the same origin (0,0)
-    last_img.paste(Image.open('paddle.jpg'), (8,8)) #24*24
-    last_img.paste(Image.open('paddle.jpg'), (8,36)) #ball
-    last_img.paste(Image.open('paddle.jpg'), (96,8)) #clock
-    last_img.paste(Image.open('paddle.jpg'), (96,36)) #round
+# def _update_triggerless():
+#     time.sleep(2)
+#     while True:
+#         if ui_state == 3 or ui_state == 4:
+#             update_display_partial(4, time.strftime('%H:%M:%S'), time_font)
+#         time.sleep(1)
+def render_welcome_screen():
+    bg = Image.new('1', (epd2in13d.EPD_HEIGHT, epd2in13d.EPD_WIDTH), 255)
+    dr = ImageDraw.Draw(bg)
+    dr.text((36,36), 'Keep your paddle flat...', font=title_small, fill=0)
     with dev_lock:
-        epd.display(epd.getbuffer(last_img))
-
+        epd.display(epd.getbuffer(bg))
+def render_profile_screen(user_id=0):
+    bg = Image.new('1', (epd2in13d.EPD_HEIGHT, epd2in13d.EPD_WIDTH), 255)
+    dr = ImageDraw.Draw(bg)
+    dr.text((8,8), 'User Profiles', font=field_font, fill=0)
+    for i in range(5):
+        if i == user_id:
+            dr.ellipse((8+i*(24+12), 36, 32+i*24, 36), outline='black', fill=0)
+        else:
+            dr.ellipse((8+i*(24+12), 36, 32+i*24, 36), outline='black')
+    with dev_lock:
+        epd.display(epd.getbuffer(bg))
+def render_train_screen():
+    bg = Image.new('1', (epd2in13d.EPD_HEIGHT, epd2in13d.EPD_WIDTH), 255)
+    dr = ImageDraw.Draw(bg)
+    dr.text((36,36), 'Keep swinging while', font=title_small, fill=0)
+    dr.text((36,72), 'we learn from you...', font=title_small, fill=0)
+    with dev_lock:
+        epd.display(epd.getbuffer(bg))
+def render_run_screen():
+    bg = Image.new('1', (epd2in13d.EPD_HEIGHT, epd2in13d.EPD_WIDTH), 255)
+    bg.paste(Image.open('paddle.jpg'), (8,8)) #24*24
+    bg.paste(Image.open('wave.jpg'), (8,36)) #ball
+    bg.paste(Image.open('swing.jpg'), (96,8)) #clock
+    bg.paste(Image.open('thumb.jpg'), (96,36)) #round
+    update_display_partial(5, "Playing")
+    
+def render_pause_screen():
+    update_display_partial(5, "Paused")
 
 def update_display_partial(field_id, field_val, field_font=field_font): # num, text
     # multi threads accessing update_field, will cause "roll-back" hazard if without lock!
@@ -45,15 +75,7 @@ def update_display_partial(field_id, field_val, field_font=field_font): # num, t
         except:
             print('gg')
     print('updated field {0} with val {1}'.format(field_id, field_val))
-
-def _update_triggerless():
-    time.sleep(2)
-    while True:
-        update_display_partial(4, time.strftime('%H:%M:%S'), time_font)
-        time.sleep(1)
-
-_render_template()
-threading.Thread(target=_update_triggerless).start()
+# threading.Thread(target=_update_triggerless).start()
 
 
 
